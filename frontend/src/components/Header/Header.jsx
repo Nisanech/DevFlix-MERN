c860529221698ca28c4e4314e90bbb8103e81c84
Edward Melo <edwardalejandromelog@gmail.com>
2023-01-30 16:27:55 -0500

    header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import "./Header.css"
+import { CiUser } from 'react-icons/ci';
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='head'>
+        <div className='info'>
+            
+            <div className='icon'>
+                <CiUser className='icongr'></CiUser> 
+            </div>
+            <div>
+                <h1> Name</h1>
+                <h2> Administrador</h2>
+            </div>
+        </div>
+        
+        <div className=''>
+            <button className='add'> + Agregar </button>
+        </div>
+    </header>
   )
 }
 
