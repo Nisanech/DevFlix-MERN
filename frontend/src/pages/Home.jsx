5a11391b0b2ee2aa36f300d64b59ecc2d7896c0b
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-01-31 11:03:24 -0500

    Página home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+// Componentes
+import Header from '../components/Header/Header'
+import ListMovies from '../components/ListMovies/ListMovies'
+import CardMovie from '../components/CardMovie/CardMovie'
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header />
+      <CardMovie/>
+      <ListMovies/>
+    </>
+  )
 };
 
 export default Home;
