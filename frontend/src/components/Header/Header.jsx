e2878e9914ee79bcab52b6ffe95ac105c6c3c358
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2023-02-01 11:42:01 -0500

    subiendo estilos

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import "./Header.css"
-import { CiUser } from 'react-icons/ci';
+import React from "react";
+import "./Header.css";
+import { CiUser } from "react-icons/ci";
 import img from "./logoblanco.png";
+import Swal from 'sweetalert2'
 
 const Header = () => {
-    
+
+  
   return (
-    <header className='head'> 
-            
-        <div className='conta'>
-                <div className='logo'>
-                    <img className="logoDevflix"src={img} alt="imagen logo peliculas" srcset="" />
-                </div>
-            
-            <div className='info'>
-                
-                <div className='icon'>
-                    <CiUser className='icongr'></CiUser> 
-                </div>
-                <div>
-                    <h1> Name</h1>
-                    <h2> Administrador</h2>
-                </div>
-            </div>
-            
-            <div className=''>
-                <button className='add'> + Agregar </button>
-            </div>
+    <header className="head">
+      <div className="conta">
+        <div className="logo">
+          <img
+            className="logoDevflix"
+            src={img}
+            alt="imagen logo peliculas"
+            srcset=""
+          />
         </div>
+
+        <div className="info">
+          <div className="icon">
+            <CiUser className="icongr"></CiUser>
+          </div>
+          <div>
+            <h1> Name</h1>
+            <h2> Administrador</h2>
+          </div>
+        </div>
+
+        <div className="">
+          <button
+            className="add">
+            + Agregar
+          </button>
+        </div>
+      </div>
     </header>
   );
 };
